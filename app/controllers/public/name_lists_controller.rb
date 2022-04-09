a874ff5be2ccea006a1523a254b1d66a6e736c7c
nknsn <nknsn21@gmail.com>
2022-04-09 17:50:15 +0000

    [Add]namelist/index/view :q git commit -m [Add]namelist/index/view exit

--- app/controllers/public/name_lists_controller.rb
+++ app/controllers/public/name_lists_controller.rb
@@ -1,2 +1,29 @@
 class Public::NameListsController < ApplicationController
+
+
+  def index
+    @name_list = NameList.new
+    @name_lists = NameList.all
+  end
+
+
+  def create
+    @name_list = NameList.new(name_list_params)
+    @name_list.user_id = current_user.id
+    if @name_list.save
+      redirect_to name_list_index_path
+      flash[:notice]='You have created book successfully'
+    else
+      @name_lists = NameList.all
+      render :index
+    end
+  end
+
+
+  private
+
+  def name_list_params
+    params.require(:name_list).permit(:name)
+  end
+
 end
